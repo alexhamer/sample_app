bb18e962992c075e9e679c7b7c263e20e06206c1
Alex Hamer <alex@hamer.ca>
2011-04-24 21:09:23 -0700

    Page 253 added Factories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,72 @@
 # == Schema Information
-# Schema version: 20110411152244
+# Schema version: 20110419210346
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password #ALEX reates getting and setter methods to manipulate passwords
+  attr_accessible :name, :email, :password, :password_confirmation
   
   #validates(:name, :presence => true)
   
   #name
   validates_presence_of :name, :on => :create, :message => "can't be blank mother fucker"
-  validates_length_of :name, :within => 3..50, :on => :create, :message => "must be present"
+  validates_length_of :name, :within => 3..50, :on => :create, :message => "must be withint 3 to 50 characters"
 
-  #email
+  #email 
   validates_presence_of :email, :on => :create, :message => "can't be blank asswipe"
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email, :with => email_regex, :on => :create, :message => "is invalid"
   validates_uniqueness_of :email, :on => :create, :message => "must be unique", :case_sensitive => false
+  
+  #password
+  validates_presence_of :password, :on => :create, :message => "can't be blank"
+  validates_confirmation_of :password, :on => :create, :message => "should match confirmation"
+  validates_length_of :password, :within => 6..40, :on => :create, :message => "must be within 6 to 40 characters"
+
+  before_save :encrypt_password
+  
+  #Return true if the user's password matched the submitted password
+  def has_password?(submitted_password)
+    #compare encrypted_password with the encrypted version of submitted_password
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password) #refers to class (class level method) itself could do class << self 
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password) #could also use (self.password)
+      #above if you use only encryypted_password not self.encrypted_password it would create a local variable
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
+    
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,15 +3,38 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
-  describe "GET 'new'" do
+#  def show
+#    @user = User.find(params[:id])
+#  end
+
+
+  describe "GET 'show'" do
+ 
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
     it "should be successful" do
-      get 'new'
+      get :show, :id => @user
       response.should be_success
     end
     
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+       
+  end
 
+  describe "GET 'new'" do
+
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+    
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
